a6a94e5806bc8e530442d0027fbbeca2124cf419
小陆斑比 <1126458447@qq.com>
2021-08-08 14:14:54 +0800

    新增获取核酸检测点接口并格式化代码

---
+++ db/acidarea_db.js
@@ -0,0 +1,68 @@
+// miniprogram/db/acidarea_db.js
+const Promise = require('../promise/es6-promise.min.js')
+const CloudDB = require("../promise/wxCloudDB.js")
+const CloudFun = require("../promise/wxCloudFun.js")
+const API = require("../promise/wxAPI.js")
+const Util = require("../utils/util.js")
+
+/**
+ * 获取指定省份、城市、地区的核酸检测地列表
+ * @param {string} province 省份
+ * @param {string} city 城市
+ * @param {string} district 地区
+ */
+function getAcidareaList(province = '江苏省', city = '南京市', district = '') {
+    return new Promise(function (resolve, reject) {
+        let data = null; // 请求参数
+        let flg = district != '' && district != '全部'; // 是否需要过滤地区
+        if (city == '全部') {
+            data = {
+                province: province
+            }
+        } else {
+            data = {
+                city: city
+            }
+        }
+        // 网络请求
+        API.Request('https://njtech.bamlubi.cn/get_detection_site', data, 'GET', `获取核酸检测点:${province}${city}${district}`).then(res => {
+            let _markers = [];
+            for (let item of res) {
+                // 判断是否需要过滤地区
+                if (flg && item["district"] != district) continue;
+                // 如果经纬度超过范围或者为空则跳过
+                if (!isLegalCoor(item["lat"], item["lng"])) continue;
+                // 构造marker数据
+                var point = new Util.createPoit(item["flag"], item["lat"], item["lng"], item["name"], 0);
+                point.address = item["address"];
+                point.tel = item["tel"];
+                _markers.push(point);
+            }
+            // 回调
+            resolve(_markers)
+        }).catch(err => {
+            reject(err)
+        })
+    })
+}
+
+/**
+ * 判断是否是合法的坐标
+ * @param {string | number} lat 纬度
+ * @param {string | number} lng 经度
+ */
+function isLegalCoor(lat, lng) {
+    // 判断为空
+    if (lat == null || lng == null || lat == '' || lng == '') return false;
+    // 先格式化为浮点型
+    lat = typeof (lat) == 'string' ? parseFloat(lat) : lat;
+    lng = typeof (lng) == 'string' ? parseFloat(lng) : lat;
+    // 判断是否合法
+    if (lat < -90 || lat > 90) return false;
+    if (lng < -180 || lng > 180) return false;
+    return true;
+}
+
+module.exports = {
+    getAcidareaList
+}
--- pages/acid_area/acid_area.js
+++ pages/acid_area/acid_area.js
@@ -2,284 +2,259 @@
 const app = getApp()
 const util = require('../../utils/util.js')
 const API = require("../../promise/wxAPI.js")
-var p_data=require('./data.js')
+const AcidareaDB = require("../../db/acidarea_db.js")
 Page({
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
-        markers: [],
-        hasMarkers: false,
-        view_text: "",
-        address: "",
-        distance: "",
-        title: "",
-        la: "",
-        ln: "",
-        notShowLabel:true,
-        distance_list:[],
-        scale:10,
-        tele:"",
-        viewShowed: false, //显示结果view的状态
-        inputVal: "", // 搜索框值
-        serachList: [], //搜索渲染推荐数据
-        // 地区选择器
-        region: ['江苏省', '南京市', '全部'],
-        customItem: '全部'
-    },
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    markers: [],
+    hasMarkers: false,
+    view_text: "",
+    address: "",
+    distance: "",
+    title: "",
+    la: "",
+    ln: "",
+    notShowLabel: true,
+    distance_list: [],
+    scale: 10,
+    tele: "",
+    viewShowed: false, //显示结果view的状态
+    inputVal: "", // 搜索框值
+    serachList: [], //搜索渲染推荐数据
+    // 地区选择器
+    region: ['江苏省', '南京市', '全部'],
+    customItem: '全部'
+  },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-        let that = this;
-        // 显示loading框
-        wx.showLoading({
-            title: '加载中',
-        })
-        // // 获取地图标记点数据
-        // API.Request('https://njtech.bamlubi.cn/getdata_auto', {
-        //     city: "南京市"
-        // }, 'GET', '获取风险地区').then(res => {
-        //     let _markers = [];
-        //     for (let item of res) {
-        //         let type = 2;
-        //         if (item["risk_level"] == "高风险") {
-        //             type = 0;
-        //         } else if (item["risk_level"] == "中风险") {
-        //             type = 1;
-        //         }
-        //         let point = new util.createPoit(item["flag"], item["lat"], item["lng"], item["title"], type);
-        //         point.address = item["address"];
-        //         point.risk_level = item["risk_level"];
-        //         _markers.push(point);
-        //     }
-        //     that.setData({
-        //         markers: _markers,
-        //         hasMarkers: true
-        //     })
-        //     // 获取用户位置
-        //     return wx.getLocation({
-        //         type: 'wgs84'
-        //     })
-        // }).then(res => {
-        //     that.setData({
-        //         la: res.latitude,
-        //         ln: res.longitude
-        //     })
-        //     this.mapCtx.moveToLocation({
-        //         latitude: parseFloat(this.data.la),
-        //         longitude: parseFloat(this.data.ln),
-        //     });
-        //     // 隐藏加载框
-        //     wx.hideLoading()
-        // }).catch(err => {
-        //     // 隐藏加载框
-        //     wx.hideLoading()
-        //     API.ShowToast('定位失败','error')
-        //     console.error(err);
-        // })
-        // 获取地图标记点数据
-        var _markers=this.getAcidareaList("江苏省","南京市","全部")
-        that.setData({
-            markers:_markers,
-            hasMarkers:true
-        })
-        return wx.getLocation({
-                type: 'wgs84'
-            }).then(res => {
-                that.setData({
-                    la: res.latitude,
-                    ln: res.longitude
-                })
-                this.mapCtx.moveToLocation({
-                    latitude: parseFloat(this.data.la),
-                    longitude: parseFloat(this.data.ln),
-                });
-                // 隐藏加载框
-                wx.hideLoading()
-            }).catch(err => {
-                // 隐藏加载框
-                wx.hideLoading()
-                API.ShowToast('定位失败','error')
-                console.error(err);
-            })
-        
-    },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-        // 页面渲染完成时，获取MAP组件
-        this.mapCtx = wx.createMapContext('myMap')
-        // this.mapCtx.moveToLocation()
-        // let marker=this.data.markers
-    },
-    /**
-     * 选择想要查看的地区
-     * 并对数据进行筛选
-     */
-    bindRegionChange: function (e) {
-      let that = this;
-      // 判断用户是否选择了全部省份或全部城市
-      let region = e.detail.value
-      console.log(region);
-      if(region[0] == '全部' || region[1] == '全部'){
-          API.ShowModal('','请至少选择到指定市哦！',false)
-          region = ['江苏省','南京市','全部']
-          this.setData({
-              region: region
-          })
-          return
-      }
-      // 显示loading框
-      wx.showLoading({
-          title: '加载中',
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    let that = this;
+    // 显示loading框
+    wx.showLoading({
+      title: '加载中',
+    })
+    // 获取地图标记点数据
+    AcidareaDB.getAcidareaList("江苏省", "南京市").then(res => {
+      that.setData({
+        markers: res,
+        hasMarkers: true
       })
-      // 获取核酸地区列表
-          let res=this.getAcidareaList(region[0],region[1],region[2])
-          // 如果没数据则展示
-          if (res.length == 0){
-              API.ShowToast('暂时没有数据','none');
-              region =  ['江苏省', '南京市', '全部']
-              return
-          }
-          // 地图中心转移
-          that.mapCtx.moveToLocation({
-              latitude: parseFloat(res[0].latitude),
-              longitude: parseFloat(res[0].longitude),
-          })
-          that.setData({
-              markers: res,
-              hasMarkers: true,
-              scale: 10,
-              region: region
-          })
-          wx.hideLoading({
-          })
+      // 获取用户位置
+      return wx.getLocation({
+        type: 'wgs84'
+      })
+    }).then(res => {
+      // 不需要实时渲染的数据，尽量不使用this.setData
+      that.data.la = res.latitude;
+      that.data.ln = res.longitude;
+      // map组件渲染完成时，获取MAP组件，并移动到用户所在位置
+      that.mapCtx = wx.createMapContext('myMap')
+      that.mapCtx.moveToLocation({
+        latitude: parseFloat(res.latitude),
+        longitude: parseFloat(res.longitude),
+      })
+      // 隐藏加载框
+      wx.hideLoading()
+    }).catch(err => {
+      // 隐藏加载框
+      wx.hideLoading()
+      API.ShowToast('定位失败', 'error')
+      console.error(err);
+    })
   },
-  getAcidareaList:function(province='江苏省',city='南京市',district='全部'){
-    var _markers=[]
-    if(city=='南京市'||city=='扬州市'){
-        for(let item of p_data.postData.RECORDS){
-          if((item["city"]==city&&district=="全部")||(item["city"]==city&&item["district"]==district)){
-            var point = new util.createPoit(item["uuid"], item["lat"], item["lng"], item["name"], 0)
-            point.address = item["address"]
-            point.tel=item["tel"]
-            _markers.push(point)
-            // console.log(_markers)
-          }
-        }
-    }
-    // console.log(_markers)
-    return _markers
-  },
-    /**
-     *点击定位按钮，移动视图到现在所在位置
-     */
-    locate:function(){
-        this.mapCtx.moveToLocation({
-            latitude: parseFloat(this.data.la),
-            longitude: parseFloat(this.data.ln),
-        });
-        // console.log(this.data.la)
-        this.setData({
-            scale:10
-        })
-    },
-    /**
-     * 点击marker
-     * @param {*} e 
-     */
-    markertap(e) {
-        // 注意markerId并不等于序号
-        let marker = this.data.markers.filter(function (x) {
-            return x.id == e.detail.markerId;
-        })[0];
-        // console.log(marker);
-        // 移动视图中心
-        this.mapCtx.moveToLocation({
-            latitude: parseFloat(marker.latitude),
-            longitude: parseFloat(marker.longitude),
-        });
-        // 设置显示信息
-        this.setData({
-            address: marker.address,
-            title: marker.title,
-            distance: util.getDistance(parseFloat(this.data.la), parseFloat(this.data.ln), parseFloat(marker.latitude), parseFloat(marker.longitude)),
-            notShowLabel: false,
-            tele:marker.tel
-        })
-    },
 
-    /**
-     * 弹窗
-     * 进行距离计算排序
-     */
-    popup(){
-        let marker=this.data.markers
-        var list_temp=[]
-        // 获取每个点到当前位置的距离
-        for(var i=0;i<marker.length;i++){
-            let dis_obj=new Object()
-            dis_obj.index=i
-            dis_obj.address=marker[i].title
-            dis_obj.distance=parseFloat(util.getDistance(parseFloat(this.data.la), parseFloat(this.data.ln), parseFloat(marker[i].latitude), parseFloat(marker[i].longitude)))
-            list_temp.push(dis_obj)
-        }
-        this.setData({
-            distance_list:list_temp
-        })
-        // 排序
-        var property = "distance";
-        var self = this;
-        var arr = self.data.distance_list;
-        var sortRule = true; // 正序倒序
-        self.setData({
-            distance_list: arr.sort(self.compare(property, sortRule))
-        })
-        // console.log(this.data.distance_list)
-        // console.log(list_temp)
-        this.selectComponent('#bottomFrame').showFrame();
-      },
-    compare: function (property, bol) {
-        return function (a, b) {
-        var value1 = a[property];
-        var value2 = b[property];
-        if(bol){
-          return value1 - value2;
-        }else {
-          return value2 - value1;
-        }
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {},
+
+  /**
+   * 选择想要查看的地区
+   * 并对数据进行筛选
+   */
+  bindRegionChange: function (e) {
+    let that = this;
+    // 判断用户是否选择了全部省份或全部城市
+    let region = e.detail.value
+    console.log(region);
+    if (region[0] == '全部' || region[1] == '全部') {
+      API.ShowModal('', '请至少选择到指定市哦！', false)
+      region = ['江苏省', '南京市', '全部']
+      this.setData({
+        region: region
+      })
+      return
+    }
+    // 显示loading框
+    wx.showLoading({
+      title: '加载中',
+    })
+    // 获取核酸地区列表
+    AcidareaDB.getAcidareaList(...region).then(res => {
+      // 如果没数据则展示
+      if (res.length == 0) {
+        API.ShowToast('暂时没有数据', 'none');
+        region = ['江苏省', '南京市', '全部']
+        return
       }
-    },
-    // 点击text，地图中心移动到该点
-    toMarker:function(e){
-        var index=e.currentTarget.dataset.item_index
-        // console.log(e.currentTarget.dataset.item_index)
-        this.mapCtx.moveToLocation({
-            latitude: parseFloat(this.data.markers[index].latitude),
-            longitude: parseFloat(this.data.markers[index].longitude),
-        });
-        this.setData({
-            scale:15
-        })
-    },
-        /**
-     * 搜索框
-     * 进行模糊搜索,点击跳转到对应点
-     */
-    // 隐藏搜索框样式
-  hideInput: function() {
+      // 地图中心转移
+      that.mapCtx.moveToLocation({
+        latitude: parseFloat(res[0].latitude),
+        longitude: parseFloat(res[0].longitude),
+      })
+      that.setData({
+        markers: res,
+        hasMarkers: true,
+        scale: 10,
+        region: region
+      })
+    }).then(res => {
+      // 隐藏loading框
+      wx.hideLoading()
+      // 同步视图
+      this.setData({
+        region: region
+      })
+    }).catch(err => {
+      // 隐藏loading框
+      wx.hideLoading()
+      // 显示查询失败
+      API.ShowToast('查询失败', 'error')
+      console.error(err);
+    })
+  },
+
+  /**
+   *点击定位按钮，移动视图到现在所在位置
+   */
+  locate: function () {
+    let that = this
+    wx.showLoading({
+      title: '定位中',
+    })
+    // 获取用户最新位置
+    wx.getLocation({
+      type: 'wgs84'
+    }).then(res => {
+      console.log(res);
+      // 不需要实时渲染的数据，尽量不使用this.setData
+      that.data.la = res.latitude
+      that.data.ln = res.longitude
+      // 移动位置
+      return that.mapCtx.moveToLocation({
+        latitude: parseFloat(res.latitude),
+        longitude: parseFloat(res.longitude),
+      })
+    }).then(res => {
+      wx.hideLoading()
+      that.setData({
+        notShowLabel: true,
+        scale: 10
+      })
+    }).catch(err => {
+      wx.hideLoading()
+      API.ShowToast('定位失败', 'error')
+    })
+  },
+
+  /**
+   * 点击marker
+   * @param {*} e 
+   */
+  markertap(e) {
+    // 注意markerId并不等于序号
+    let marker = this.data.markers.filter(function (x) {
+      return x.id == e.detail.markerId;
+    })[0];
+    console.log(marker);
+    // 移动视图中心
+    this.mapCtx.moveToLocation({
+      latitude: parseFloat(marker.latitude),
+      longitude: parseFloat(marker.longitude),
+    });
+    // 设置显示信息
+    this.setData({
+      address: marker.address,
+      title: marker.title,
+      distance: util.getDistance(parseFloat(this.data.la), parseFloat(this.data.ln), parseFloat(marker.latitude), parseFloat(marker.longitude)),
+      notShowLabel: false,
+      tele: marker.tel
+    })
+  },
+
+  /**
+   * 弹窗
+   * 进行距离计算排序
+   */
+  popup() {
+    let marker = this.data.markers
+    var list_temp = []
+    // 获取每个点到当前位置的距离
+    for (var i = 0; i < marker.length; i++) {
+      let dis_obj = new Object()
+      dis_obj.index = i
+      dis_obj.address = marker[i].title
+      dis_obj.distance = parseFloat(util.getDistance(parseFloat(this.data.la), parseFloat(this.data.ln), parseFloat(marker[i].latitude), parseFloat(marker[i].longitude)))
+      list_temp.push(dis_obj)
+    }
+    this.setData({
+      distance_list: list_temp
+    })
+    // 排序
+    var property = "distance";
+    var self = this;
+    var arr = self.data.distance_list;
+    var sortRule = true; // 正序倒序
+    self.setData({
+      distance_list: arr.sort(self.compare(property, sortRule))
+    })
+    // console.log(this.data.distance_list)
+    // console.log(list_temp)
+    this.selectComponent('#bottomFrame').showFrame();
+  },
+  compare: function (property, bol) {
+    return function (a, b) {
+      var value1 = a[property];
+      var value2 = b[property];
+      if (bol) {
+        return value1 - value2;
+      } else {
+        return value2 - value1;
+      }
+    }
+  },
+  // 点击text，地图中心移动到该点
+  toMarker: function (e) {
+    var index = e.currentTarget.dataset.item_index
+    // console.log(e.currentTarget.dataset.item_index)
+    this.mapCtx.moveToLocation({
+      latitude: parseFloat(this.data.markers[index].latitude),
+      longitude: parseFloat(this.data.markers[index].longitude),
+    });
+    this.setData({
+      scale: 15
+    })
+  },
+
+  /**
+   * 搜索框
+   * 进行模糊搜索,点击跳转到对应点
+   */
+  // 隐藏搜索框样式
+  hideInput: function () {
     this.setData({
       inputVal: "",
       viewShowed: false,
     });
   },
+
   // 键盘抬起事件2
-  inputTyping: function(e) {
+  inputTyping: function (e) {
     // console.log("input-----",e)
     var value = e.detail.value
     var that = this;
@@ -289,14 +264,14 @@ Page({
         viewShowed: false,
       });
     } else {
-    //“这里需要特别注意，不然在选中下拉框值的时候，下拉框又出现”
+      //“这里需要特别注意，不然在选中下拉框值的时候，下拉框又出现”
       if (e.detail.cursor) { //e.detail.cursor表示input值当前焦点所在的位置
         var arr = [];
         for (var i = 0; i < markers.length; i++) {
           if (markers[i].title.indexOf(value) >= 0) {
-            let marker_obj=new Object()
-            marker_obj.marker_index=i;  //对应markers数组下标
-            marker_obj.addr=markers[i].title
+            let marker_obj = new Object()
+            marker_obj.marker_index = i; //对应markers数组下标
+            marker_obj.addr = markers[i].title
             arr.push(marker_obj);
           }
         }
@@ -308,32 +283,34 @@ Page({
       }
     }
   },
+
   // 点击搜索列表中的结果
-  name: function(res) {
+  name: function (res) {
     // console.log(res.currentTarget.dataset.index);
     var index = res.currentTarget.dataset.index;
     var that = this;
-    let marker=that.data.markers[index];
+    let marker = that.data.markers[index];
     that.setData({
       inputVal: marker.title,
       viewShowed: false,
-      scale:15,
+      scale: 15,
       address: marker.address,
       title: marker.title,
       distance: util.getDistance(parseFloat(this.data.la), parseFloat(this.data.ln), parseFloat(marker.latitude), parseFloat(marker.longitude)),
       notShowLabel: false,
-      tele:marker.tel
+      tele: marker.tel
     })
     //移动试图到搜索到的点
     this.mapCtx.moveToLocation({
-        latitude: parseFloat(marker.latitude),
-        longitude: parseFloat(marker.longitude),
+      latitude: parseFloat(marker.latitude),
+      longitude: parseFloat(marker.longitude),
     });
   },
-  //隐藏信息框
+
+  // 隐藏信息框
   hideLabel: function () {
     this.setData({
-        notShowLabel: true
+      notShowLabel: true
     })
   }
 })
