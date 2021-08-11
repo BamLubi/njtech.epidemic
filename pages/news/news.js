86abcd290409791ca60ba77f729d12e39c090eb6
liutongdaren <73803728+1351814021@users.noreply.github.com>
2021-08-11 21:31:37 +0800

    Update news.js

--- pages/news/news.js
+++ pages/news/news.js
@@ -2,7 +2,6 @@
 const app = getApp()
 const util = require('../../utils/util.js')
 const API = require("../../promise/wxAPI.js")
-const NewsDB = require("../../db/news_db.js")
 import {province} from "../../utils/province.js"
 Page({
   /**
@@ -10,23 +9,43 @@ Page({
    */
   data: {
     news: [],
-    provinces: province,
-    select: 0,
-    type: 1,// 获取新闻类型，1为省、2为市
-    postdata: '',// 网络请求时附带参数，省份名称或城市名称
-    hasMoreNews: true,
-    isLoading: false,
-    inputVal: ''
+    provinces:province,
+    myProvince:'江苏省',
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
+
+   bindPickerChange: function(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value,
+    })
+    this.onLoad()
+  },
+
+  getData:function(myProvince='江苏省'){
+    wx.request({
+      url: 'https://njtech.bamlubi.cn/get_news_data',
+      data:{
+        page:1,
+        num:10,
+        province:myProvince,
+      },
+      success:res=>{
+        console.log(res.data)
+        this.setData({
+          news:res.data
+        })
+      }
+    })
+   },
+
   onLoad: function (options) {
-    // 设置请求内容
-    this.data.postdata = this.data.provinces[this.data.select].postName
-    // 获取新闻
-    this.getNewsList()
+    //获取该省新闻
+    this.getData(this.data.provinces[this.data.index].postName);
   },
 
   /**
@@ -61,60 +80,27 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    // 置空源数据
-    this.setData({
-      news: []
-    })
-    // 获取新闻
-    this.getNewsList().then(res => {
-      wx.stopPullDownRefresh()
-    })
+    wx.showNavigationBarLoading() //在标题栏中显示加载
+      //模拟加载
+      setTimeout(function(){
+      // complete
+      wx.hideNavigationBarLoading() //完成停止加载
+      wx.stopPullDownRefresh() //停止下拉刷新
+      },1500);
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    // 如果无数据了就不要再发请求了
-    if (!this.data.hasMoreNews) return;
-    // 节流
-    if (!this.data.isLoading) {
-      this.getNewsList()
-    }
+
   },
 
   /**
-   * 获取新闻列表
+   * 用户点击右上角分享
    */
-  getNewsList: function () {
-    let that = this
-    let num = 12
-    //获取该省新闻
-    wx.showLoading({
-      title: '加载中',
-    })
-    this.setData({
-      isLoading: true
-    })
-    return NewsDB.getNewsList(this.data.news.length, num, this.data.type, this.data.postdata).then(res => {
-      // 更新视图
-      that.setData({
-        news: that.data.news.concat(res)
-      })
-      if (res.length < num) {
-        that.setData({
-          hasMoreNews: false
-        })
-      }
-      that.setData({
-        isLoading: false
-      })
-      wx.hideLoading()
-    }).catch(err => {
-      wx.hideLoading()
-      API.ShowToast('网络请求失败', 'error')
-      console.log(err)
-    })
+  onShareAppMessage: function () {
+
   },
 
   /**
@@ -128,60 +114,6 @@ Page({
     let url = "/pages/news_detail/news_detail?" + "data=" + JSON.stringify(data);
     wx.navigateTo({
       url: url
-    })
-  },
-
-  /**
-   * 省份切换
-   * @param {*} e 
-   */
-  bindPickerChange: function (e) {
-    this.setData({
-      select: e.detail.value,
-      news: [],
-      hasMoreNews: true
-    })
-    // 设置为获取省
-    this.data.type = 1
-    // 设置请求内容
-    this.data.postdata = this.data.provinces[this.data.select].postName
-    // 获取新闻
-    this.getNewsList()
-  },
-
-  search: function(e){
-    let city = e.detail.value
-    // 如果没输入则返回
-    if (city == null || city == ''){
-      return
-    }
-    // 判断用户是否输入“市”
-    if(city.indexOf("市") == -1){
-      city += '市'
-    }
-    // 判断用户输入了市，但是还包含其他内容
-    if (city.indexOf("市") != city.length - 1){
-      API.ShowModal('','请按如下格式输入城市：南京市',false)
-      return
-    }
-    // 设置为获取市
-    this.data.type = 2
-    // 设置请求内容
-    this.data.postdata = city
-    // 置空源数据
-    this.setData({
-      news: []
-    }) 
-    // 发起网络请求
-    this.getNewsList()
-  },
-
-  /**
-   * 清除输入框内容
-   */
-  hideInput: function(){
-    this.setData({
-      inputVal: ""
     })
   }
 })
